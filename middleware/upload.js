217cb544b93a4196c3fe211dd0b002e3798763ce
Sargis <sargis.strong@gmail.com>
2021-11-12 14:55:14 +0400

    change cv types

--- middleware/upload.js
+++ middleware/upload.js
@@ -3,10 +3,11 @@ const {GridFsStorage} = require('multer-gridfs-storage');
 const keys = require('../config/keys')
 
 const storage = new GridFsStorage({
-    url: keys.mongoHR_Tool,
+    url: keys.mongoHR_Tool3,
     options: { useNewUrlParser: true, useUnifiedTopology: true },
     file: (req, file) => {
-        const match = ["image/png", "image/jpeg", "application/docx", "application/pdf"];
+        
+        const match = ["image/png", "image/jpeg", "application/docx", "application/doc", "application/pdf"];
 
         if (match.indexOf(file.mimetype) === -1) {
             const filename = `${Date.now()}-any-name-${file.originalname}`;
